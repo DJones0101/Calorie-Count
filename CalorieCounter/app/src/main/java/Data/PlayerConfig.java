4a60447a351287ba74e1fb85dbddf3366dd0e119
Darius Jones <36016190+DJones0101@users.noreply.github.com>
2019-04-26 13:35:47 -0700

    Update PlayerConfig.java

--- CalorieCounter/app/src/main/java/Data/PlayerConfig.java
+++ CalorieCounter/app/src/main/java/Data/PlayerConfig.java
@@ -2,7 +2,7 @@ package Data;
 
 public class PlayerConfig {
 
-    public static final String API_KEY = "AIzaSyBcUVqSdUXqPgLoSjPGz9WqICaFUtbH6UI";
+    public static final String API_KEY = "";
 
     public void PlayerConfig(){}
 }
